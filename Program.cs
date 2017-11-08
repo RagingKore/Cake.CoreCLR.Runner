244f8acff178ccd4619402c21e7c2acf79d90172
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-11-08 12:50:15 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ namespace Cake.CoreCLR.Runner
 {
     public static class Program
     {
-        public static int Main() => return Cake.Main();
+        public static int Main() => Cake.Main();
     }
 }
